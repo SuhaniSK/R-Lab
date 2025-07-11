742e8723fcaf63a809ba183ea2153eb543b8a8f3
Suhani S K <156022230+SuhaniSK@users.noreply.github.com>
2025-07-11 09:59:32 +0530

    Update 2.r

--- 2.r
+++ 2.r
@@ -1,10 +1,14 @@
 library(ggplot2)
 data(mtcars)
-ggplot(data=mtcars,aes(sample=mpg))+
+head(mtcars)
+ggplot(mtcars,aes(sample=mtcars$mpg))+
 stat_qq()+
 stat_qq_line()+
 labs(
-    title="Q-Q plot of mpg",
-    x="theoretical Quantiles",
-    y="Sample mpg Quantiles"
+  title="Quartile Q-Q plot for MPG",
+  x="Theoretical Quantiles",
+  y="Sample Quantiles"
 )
+correlation<-cor.test(mtcars$mpg,mtcars$hp)
+cat("Correlation bw hp and mpg is:",round(correlation$estimate,2),"\n")
+cat("P-value of correlation test is:",round(correlation$p.value,4),"\n")
